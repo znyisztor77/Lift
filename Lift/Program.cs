dc3aec20f4e52d1435204cef57353bd153cb1f4c
Nyisztor Zoltán <znyisztor77@gmail.com>
2024-01-14 21:18:16 +0100

    6. feladatig kész.

--- Lift/Program.cs
+++ Lift/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,11 +25,88 @@ namespace Lift
             liftAdatok =  fajlBeolvasas(fajlnev);
             for(int i = 0; i < liftAdatok.Count; i++)
             {
-                Console.WriteLine($"{liftAdatok[i].idoPont}, {liftAdatok[i].kartyaSorszam}, {liftAdatok[i].indulasSzint}, {liftAdatok[1].celSzint}") ;
+                Console.WriteLine($"{liftAdatok[i].idoPont} {liftAdatok[i].kartyaSorszam} {liftAdatok[i].indulasSzint} {liftAdatok[i].celSzint}") ;
             }
 
-
+            Console.Write("3. feladat: ");
+            Console.WriteLine($"Az összes lift használat: {liftAdatok.Count}");
+            Console.Write("4. feladat: ");
+            Console.WriteLine($"Időszak {liftAdatok[0].idoPont} - {liftAdatok[(liftAdatok.Count)-1].idoPont}");
+            Console.Write($"5. feladat: ");           
+            maxCelSzint(liftAdatok);
+            Console.WriteLine("6. feladat: ");
+            int kartya_szam = adatbeker("\t Kérem a kártya számát: ");
+            int celszint = adatbeker("\t Kérem a szintet: ");
+            utazasKeres(liftAdatok, kartya_szam, celszint);
+            statisztika(liftAdatok);
             Console.ReadKey();
+        }
+
+        private static void statisztika(List<Lift> liftAdatok)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void utazasKeres(List<Lift> liftAdatok, int kartya_szam, int celszint)
+        {
+            bool l = false;
+            for(int i = 0; i< liftAdatok.Count; i++)
+            {
+                if (liftAdatok[i].kartyaSorszam == kartya_szam && liftAdatok[i].celSzint == celszint)
+                {
+                    l = true; 
+                }
+                
+                
+            }
+            if (l)
+            {
+                Console.WriteLine($"A(z) {kartya_szam} kártyával utaztak a {celszint}. emeletre");
+            }
+            else
+            {
+                Console.WriteLine($"A(z) {kartya_szam} kártyával nem utaztak a {celszint}. emeletre");
+            }
+
+            
+        }
+
+        private static int adatbeker(string v)
+        {
+            int szam;
+            while (true)
+            {
+                Console.Write(v);
+                try
+                {
+                    szam = Convert.ToInt32(Console.ReadLine());
+                    break;
+
+                }
+                catch 
+                {
+
+                   szam = 5; break;
+                }
+
+            }
+            return szam;
+
+        }
+
+        private static void maxCelSzint(List<Lift> liftAdatok)
+        {
+            
+            int max_cel = liftAdatok[0].celSzint;
+            for(int i = 0; i< liftAdatok.Count; i++)
+            {
+                if (liftAdatok[i].celSzint> max_cel)
+                {
+                    max_cel = liftAdatok[i].celSzint;
+                }
+            }
+
+            Console.WriteLine($"Célszint max: {max_cel}");
         }
 
         private static List<Lift> fajlBeolvasas(string fajlnev)
@@ -49,7 +127,7 @@ namespace Lift
             
             
             
-            sr.ReadToEnd();
+            
             return liftadatok;
         }
     }
